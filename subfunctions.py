09298fe81ae1301c92ee478fa7d5ad69e936e353
EvanWarrick <114360240+EvanWarrick@users.noreply.github.com>
2022-09-26 15:44:36 -0500

    Update subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -74,52 +74,48 @@ def F_gravity(terrain_angle,rover,planet):      #define function F_gravity that 
 def F_rolling(omega,terrain_angle,rover,planet,Crr):    #define function F_rolling that receives scalar or array omega and terrain_angle, rover and planet dict, and scalar Crr
     Frr=0
     Fn=0
-    if not isinstance(rover,dict) or if not isinstance(planet,dict):
+    if not isinstance(rover,dict) or if not isinstance(planet,dict):        #check if rover or planet are not dicts
         raise Exception('Either the third or fourth argument is not a dict.')
-    if np.isscalar(omega) or isinstance(omega,np.ndarray):
-        if np.isscalar(terrain_angle): 
-            if terrain_angle < -75 or terrain_angle > 75:
+    if np.isscalar(omega) or isinstance(omega,np.ndarray):      #check if omega is a scalar or array
+        if np.isscalar(terrain_angle): #check if terrain_angle is a scalar
+            if terrain_angle < -75 or terrain_angle > 75:   #test if terrain angle is not between positive and negative 75
                 raise Exception('One of the values for the terrain angle is not between -75 and 75 degrees.')
-            Fn = get_mass(rover)*planet['g']*np.cos(terrain_angle*np.pi/180)
-        elif isinstance(terrain_angle,np.ndarray):
-            if (terrain_angle < -75).any() or (terrain_angle > 75).any():
+            Fn = get_mass(rover)*planet['g']*np.cos(terrain_angle*np.pi/180)        #calculate value of normal force
+        elif isinstance(terrain_angle,np.ndarray):  #check if terrain_angle is an array
+            if (terrain_angle < -75).any() or (terrain_angle > 75).any():   #test if any value in terrain_angle is not between positive and negative 75
                 raise Exception('One of the values for the terrain angle is not between -75 and 75 degrees.')
-            Fn = get_mass(rover)*planet['g']*np.cos(terrain_angle*np.pi/180)
+            Fn = get_mass(rover)*planet['g']*np.cos(terrain_angle*np.pi/180)    #calculate normal force
         else: raise Exception('The second argument is neither a scalar nor a vector.') 
     else: raise Exception('The first argument is neither a scalar nor a vector.')
-    if not np.isscalar(Crr) or isinstance(Crr, str):
+    if not np.isscalar(Crr) or isinstance(Crr, str):    #raise exception if Crr is a string or not a scalar
         raise Exception('The fifth argument is not a scalar.')
-    elif Crr <= 0:
+    elif Crr <= 0:  #raise exception if Crr is negative
         raise Exception('The fifth argument is not postive.')
-    Frrs = -1*Crr*Fn
-    omega_out = omega/get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
-    v_rover = omega_out*rover['wheel_assembly']['wheel']['radius']    
+    Frrs = -1*Crr*Fn    #calculate rolling resistance force
+    omega_out = omega/get_gear_ratio(rover['wheel_assembly']['speed_reducer'])  #calculate the value of the speed reducer omega output
+    v_rover = omega_out*rover['wheel_assembly']['wheel']['radius']    #calculate the tangential velocity of the rover wheels
     if isinstance(omega,np.ndarray):
         Frr=[0]
         for i in range(len(omega)):
-            Frr = erf(40*v_rover[i])*Frrs
+            Frr = erf(40*v_rover[i])*Frrs   #if the speed of the wheel is close to 0, the rolling resistance value should be 0
     else: Frr = erf(40*v_rover)*Frrs  
     return Frr
 
-def F_net(omega, terrain_angle, rover, planet, Crr):
+def F_net(omega, terrain_angle, rover, planet, Crr):    #define function F_net that receives scalar or array omega and terrain_angle, rover and planet dicts, and scalar Crr
     F=0
-    if not isinstance(rover,dict):
+    if not isinstance(rover,dict):  #test if rover is not a dict
         raise Exception('The third argument is not a dict.')
-    if not isinstance(planet,dict):
+    if not isinstance(planet,dict):     #raise exception if planet is not a dict
         raise Exception('The fourth argument is not a dict.')
-    if not np.isscalar(Crr) or isinstance(Crr, str):
+    if not np.isscalar(Crr) or isinstance(Crr, str):    #raise exception if Crr is a string or not a scalar
         raise Exception('The fifth argument is not a scalar.')
-    elif Crr <= 0:
+    elif Crr <= 0:     #check if Crr is negative
         raise Exception('The fifth argument is not postive.') 
-    if np.isscalar(omega) or isinstance(omega, np.ndarray):
-        if np.isscalar(terrain_angle):
-            if terrain_angle < -75 or terrain_angle > 75:
+    if np.isscalar(omega) or isinstance(omega, np.ndarray):     #proceed if omega is a scalar or array
+        if np.isscalar(terrain_angle) or isinstance(terrain_angle,np.ndarray):  #check if terrain_angle is a scalar or array
+            if terrain_angle < -75 or terrain_angle > 75:   #raise exception if terrain_angle is not between negative and positive 75
                 raise Exception('One of the values for the terrain angle is not between -75 and 75 degrees.')
-            F = F_drive(omega,rover) + F_gravity(terrain_angle, rover, planet) + F_rolling(omega, terrain_angle, rover, planet, Crr)
-        elif isinstance(terrain_angle,np.ndarray):
-            if (terrain_angle < -75).any() or (terrain_angle > 75).any():
-                raise Exception('One of the values for the terrain angle is not between -75 and 75 degrees.')
+            F = F_drive(omega,rover) + F_gravity(terrain_angle, rover, planet) + F_rolling(omega, terrain_angle, rover, planet, Crr)    #calculate net force
         else: raise Exception('The second argument is neither a scalar nor a vector.')
     else: raise Exception('The first argument is neither a scalar not a vector.')
-    F = F_drive(omega,rover) + F_gravity(terrain_angle,rover,planet) + F_rolling(omega,terrain_angle,rover,planet,Crr)
     return F 
