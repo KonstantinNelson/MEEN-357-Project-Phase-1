9c563b2a1440f2aa59a1fca7323c710765fba59d
EvanWarrick <114360240+EvanWarrick@users.noreply.github.com>
2022-09-27 15:52:30 -0500

    Update analysis_terrain_slope.py

--- analysis_terrain_slope.py
+++ analysis_terrain_slope.py
@@ -6,11 +6,13 @@ import matplotlib.pyplot as plt
 
 #import rover and planet dicts from define_rover
 rover, planet = define_rover_1()
+wheel_radius = rover['wheel_assembly']['wheel']['radius']
+gear_ratio=get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
 Crr = 0.2
 #generate values of slope angles
 slope_list_deg = np.linspace(-10,35,25)
 #initialize maximum omega value as array of zeros
-omega_max = np.zeros(len(slope_list_deg), dtype = float)
+v_max = np.zeros(len(slope_list_deg), dtype = float)
 #retrieve the no load speed of the motor
 omega_nl = rover['wheel_assembly']['motor']['speed_noload']
 
@@ -18,10 +20,10 @@ omega_nl = rover['wheel_assembly']['motor']['speed_noload']
 for i in range(len(slope_list_deg)):
     fun = lambda omega: F_net(omega, float(slope_list_deg[i]), rover, planet, Crr)
     sol = root_scalar(fun, method='bisect', bracket=[0, omega_nl])
-    omega_max[i] = sol.root
+    v_max[i] = sol.root*wheel_radius/gear_ratio
 
 #graph maximum rover speeds at every terrain angle
-plt.plot(slope_list_deg,omega_max)
+plt.plot(slope_list_deg,v_max)
 plt.xlabel('Terrain Angle [deg]')
 plt.ylabel('Max Rover Speed [m/s]')
 plt.show()
