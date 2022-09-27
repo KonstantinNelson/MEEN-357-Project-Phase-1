95d5b449ea2c53f0ad4b82ab5e89c471960b6d7b
EvanWarrick <114360240+EvanWarrick@users.noreply.github.com>
2022-09-27 12:21:45 -0500

    Update analysis_rolling_resistance.py

--- analysis_rolling_resistance.py
+++ analysis_rolling_resistance.py
@@ -1,35 +1,22 @@
 import numpy as np
 from subfunctions import *
-from define_rover import *
 from scipy.optimize import root_scalar
 import matplotlib.pyplot as plt
 
-
+Slope = 0 
 rover, planet = define_rover_1()
-slope = 0
-Crr_array = numpy.linspace(0.01,0.4,25)
-v_max = np.zeros(len(Crr_array), dtype = float)
-
-
-
-
-for i in range(len(Crr_array)):
-    
-    
-
-plt.plot(Crr_array,v_max)
-plt.xlabel('Coeficient of rolling resistance [degrees]')
-plt.ylabel('Maximum velocity [m/s]')
-plt.show()
-
-#old code below---------------------------------------------------------------------------------
-omega_max = np.zeros(len(slope_list_deg), dtype = float)
+Crr_list = np.linspace(0.01,0.4,25)
+omega_max = np.zeros(len(Crr_list), dtype = float)
 omega_nl = rover['wheel_assembly']['motor']['speed_noload']
 
 # find where F_net == 0
-for i in range(len(slope_list_deg)):
-    fun = lambda omega: F_net(omega, float(slope_list_deg[i]), rover, planet, Crr)
+for i in range(len(Crr_list)):
+    fun = lambda omega: F_net(omega, Slope, rover, planet,  float(Crr_list[i]))
     sol = root_scalar(fun, method='bisect', bracket=[0, omega_nl])
-    omega_max[i] = sol.root
+    omega_max[i] = sol.root #list of omega_max at each slope
+
+plt.plot(Crr_list,omega_max)
+plt.xlabel('Rolling Resistance Coefficient')
+plt.ylabel('Max Rover Speed [m/s]')
 
 
