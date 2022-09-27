8166c340cfc1077a48a30b3fed0a995bfcaa01a0
EvanWarrick <114360240+EvanWarrick@users.noreply.github.com>
2022-09-27 14:13:55 -0500

    Update analysis_rolling_resistance.py

--- analysis_rolling_resistance.py
+++ analysis_rolling_resistance.py
@@ -3,9 +3,12 @@ from subfunctions import *
 from scipy.optimize import root_scalar
 import matplotlib.pyplot as plt
 
-Slope = 0 
+Slope = 0 #set slope=0
+#import rover and planet dicts from define_rover
 rover, planet = define_rover_1()
+#create an array of rolling resistance values
 Crr_list = np.linspace(0.01,0.4,25)
+#initialize omega_max as array of zeroes
 omega_max = np.zeros(len(Crr_list), dtype = float)
 omega_nl = rover['wheel_assembly']['motor']['speed_noload']
 
@@ -15,8 +18,7 @@ for i in range(len(Crr_list)):
     sol = root_scalar(fun, method='bisect', bracket=[0, omega_nl])
     omega_max[i] = sol.root #list of omega_max at each slope
 
+#graph max speed vs rolling resistance
 plt.plot(Crr_list,omega_max)
 plt.xlabel('Rolling Resistance Coefficient')
 plt.ylabel('Max Rover Speed [m/s]')
-
-
