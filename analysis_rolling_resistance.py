f24042e1a49276ae1cb1e7fae3cde00718063ada
EvanWarrick <114360240+EvanWarrick@users.noreply.github.com>
2022-09-27 15:53:06 -0500

    Update analysis_rolling_resistance.py

--- analysis_rolling_resistance.py
+++ analysis_rolling_resistance.py
@@ -6,19 +6,22 @@ import matplotlib.pyplot as plt
 Slope = 0 #set slope=0
 #import rover and planet dicts from define_rover
 rover, planet = define_rover_1()
+wheel_radius = rover['wheel_assembly']['wheel']['radius']
+gear_ratio=get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
 #create an array of rolling resistance values
 Crr_list = np.linspace(0.01,0.4,25)
 #initialize omega_max as array of zeroes
-omega_max = np.zeros(len(Crr_list), dtype = float)
+v_max = np.zeros(len(Crr_list), dtype = float)
 omega_nl = rover['wheel_assembly']['motor']['speed_noload']
 
 # find where F_net == 0
 for i in range(len(Crr_list)):
     fun = lambda omega: F_net(omega, Slope, rover, planet,  float(Crr_list[i]))
     sol = root_scalar(fun, method='bisect', bracket=[0, omega_nl])
-    omega_max[i] = sol.root #list of omega_max at each slope
+    v_max[i] = sol.root*wheel_radius/gear_ratio #list of omega_max at each slope
+
 
 #graph max speed vs rolling resistance
-plt.plot(Crr_list,omega_max)
+plt.plot(Crr_list,v_max)
 plt.xlabel('Rolling Resistance Coefficient')
 plt.ylabel('Max Rover Speed [m/s]')
