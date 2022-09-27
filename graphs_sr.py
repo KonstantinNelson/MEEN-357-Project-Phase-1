d877a8448883a181b1cd053a3fa087a6aa8b8480
Konstantin Nelson <36679256+KonstantinNelson@users.noreply.github.com>
2022-09-27 14:38:48 -0500

    Update graphs_sr.py

--- graphs_sr.py
+++ graphs_sr.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from math import erf
 import matplotlib.pyplot as plt
@@ -7,16 +6,14 @@ from subfunctions import tau_dcmotor, get_gear_ratio
 
 #generate values of omega
 omega = np.linspace(0,3.8,25)
-
 #import rover and planet dicts from define_rover
 rover, planet = define_rover_1()
 #calculate values of tau of the speed reducer output
 tau = tau_dcmotor(omega,rover['wheel_assembly']['motor'])*get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
-motor = rover['wheel_assembly']['motor']
-#calculate the output power of the speed reducer
-P = -(motor['speed_noload']/motor['torque_stall'])*tau**2 + motor['speed_noload']*tau
 #redefine omega as speed reducer omega values
 omega=omega/get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
+#calculate the output power of the speed reducer
+P = omega*tau
 
 #graphs
 f = plt.figure(figsize=(8,7))
